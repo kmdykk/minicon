04aaf3c68062e88909b5f178f41ad4d97c5f8abb
Yukiko Komeda <34765681+kmdykk@users.noreply.github.com>
2018-02-16 04:05:25 +0000

    Improve the look of the admin interface

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,11 +1,14 @@
 class Admin::EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :event_params, only: [:create, :update]
+  
   layout 'admin'
   def index
     @events = Event.all
   end
   
   def show
-    @event = Event.find(params[:id])
+  
   end
   
   def new
@@ -13,13 +16,32 @@ class Admin::EventsController < ApplicationController
   end
   
   def create
-    event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
     @event = Event.new(event_params)
     @event.save
     redirect_to admin_event_path(@event), notice: 'Event was successfully created.' 
   end
   
   def edit
-  @event = Event.find(params[:id])
+    @event = Event.find(params[:id])
   end
+  
+  def update
+    @event.update(event_params)
+    redirect_to admin_event_path(@event), notice: 'Event was successfully updated.'
+  end
+  
+  def destroy
+    @event.destroy
+    redirect_to admin_events_path, notice: 'Event was successfully destroyed'
+  end
+  
+ private
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+  end
+ 
 end
